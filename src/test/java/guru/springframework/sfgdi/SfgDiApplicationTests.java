f6bebb2e47bd5b9fe996030b2e20c475a0c606b5
gendath <sellwithperry@gmail.com>
2022-05-13 11:02:25 -0700

    Reconfigured to java configuration with active profiles

--- src/test/java/guru/springframework/sfgdi/SfgDiApplicationTests.java
+++ src/test/java/guru/springframework/sfgdi/SfgDiApplicationTests.java
@@ -2,7 +2,9 @@ package guru.springframework.sfgdi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(basePackages = {"guru.springframework.sfgdi", "com.springframework.pets"})
 @SpringBootTest
 class SfgDiApplicationTests {
 
